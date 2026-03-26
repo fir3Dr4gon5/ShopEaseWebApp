6c50c6c5c916150ff30bc9aec9eeb41acbd22601
fir3Dr4gon5 <diarmuid@behuman.org>
2026-03-26 13:27:43 +0000

    update tables, product details need fixed not going to cart page error thrown. havent tested order confirmation

--- ShopEaseWebApp/Models/Product.cs
+++ ShopEaseWebApp/Models/Product.cs
@@ -8,6 +8,6 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int StockQuantity { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
